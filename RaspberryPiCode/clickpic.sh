ae5148febdfe01bc1259c8ae4c8db6ff754ce7c1
harish61 <58221251+harish61@users.noreply.github.com>
2021-04-17 23:51:58 +0530

    Create clickpic.sh

---
+++ RaspberryPiCode/clickpic.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -rfv /home/pi/Documents/download/Upload/*
+
+a=0
+while [ $a -lt 11 ]
+do   
+    DATE=$(date +"%Y-%m-%d_%H%M%S") 
+    echo $DATE
+    #raspistill -vf -hf -o /home/pi/Documents/download/zipthis/$DATE.jpg
+    #python3 gps.py $DATE
+    a=`expr $a + 1`
+    sleep 4
+
+done
+
+DATE2=$(date +"%Y-%m-%d_%H%M%S") 
+
+if [ -z "$(ls -A /home/pi/Documents/download/zipthis)" ]; then
+   echo "Empty"
+else
+   zip -r /home/pi/Documents/download/Upload/$DATE2.zip  /home/pi/Documents/download/zipthis
+fi
+
+rm -rfv /home/pi/Documents/download/zipthis/*
+
+rclone sync -v /home/pi/Documents/download drive:images
